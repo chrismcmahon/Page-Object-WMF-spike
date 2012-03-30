6085e460df3adfb0573d7a6fadc8e20a043d74a5
Chris McMahon <christopher.mcmahon@gmail.com>
2012-03-30 08:40:48 -0600

    add 'check links' method, proper (unused) URL for login page

--- lib/pages/login_page.rb
+++ lib/pages/login_page.rb
@@ -1,7 +1,8 @@
 class LoginPage
   include PageObject
 
-  page_url 'http://en.wikipedia.org/w/index.php?title=Special:UserLogin'
+  #page_url 'http://en.wikipedia.beta.wmflabs.org/w/index.php?title=Special:UserLogin'
+  page_url 'https://en.wikipedia.org/w/index.php?title=Special:UserLogin'
   text_field(:username, :id => 'wpName1')
   text_field(:password, :id => 'wpPassword1')
   button(:login, :id => 'wpLoginAttempt')
--- spec/login/login_spec.rb
+++ spec/login/login_spec.rb
@@ -5,12 +5,10 @@ describe "Logging into the system" do
   context "successful login" do
     it "should display welcome message" do
       visit_page(LoginPage).login_with('foo', 'bar')
+      #DON'T ACTUALLY WANT TO LOG IN UNTIL THIS IS HANDLED SANELY
       #@current_page.text.should include "Welcome foo"
       @current_page.text.should include "Secure your account"
       @current_page.text.should include "phishing"
-      @current_page.phishing.should be_empty
-      @current_page.text.should include "Not to be confused with"
-      @current_page.back
     end
   end
 
@@ -21,4 +19,16 @@ describe "Logging into the system" do
       @current_page.text.should include "Secure your account"
     end
   end
+  
+  context "visit login page links" do
+    it "should follow all the defined links" do 
+      visit_page(LoginPage)
+      @current_page.phishing.should be_empty
+      @current_page.text.should include "Not to be confused with"
+      visit_page(LoginPage)
+      @current_page.password_strength.should be_empty
+      @current_page.text.should include "measure of the effectiveness"
+    end
+  end
+  
 end
