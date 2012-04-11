90d5889581d617ccffd06d89e946b32477622f20
Chris McMahon <christopher.mcmahon@gmail.com>
2012-04-11 15:04:56 -0600

    put bogus_page in its own file

--- lib/pages/login_page.rb
+++ lib/pages/login_page.rb
@@ -16,10 +16,4 @@ class LoginPage
   end
 end
 
-class BogusPage
-  include PageObject
-  #http://en.wikipedia.beta.wmflabs.org/wiki/Bogus_page
-  page_url 'https://en.wikipedia.org/wiki/Bogus_page'
-  link(:search, :link_text => 'search for Bogus page in Wikipedia')
-  link(:search2, :link_text => 'Search for "Bogus page"')
-end
+
