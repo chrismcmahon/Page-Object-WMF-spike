4a8e9606fa75327f363a992bdd387a9e456b8f1d
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2012-05-05 15:21:12 -0500

    refactored test to remove duplicate visit_page calls

--- spec/login/login_spec.rb
+++ spec/login/login_spec.rb
@@ -2,41 +2,50 @@
 require 'spec_helper'
 
 describe "Logging into the system" do
+  before(:each) do
+    visit_page LoginPage
+  end
+
   context "successful login" do
     it "should display welcome message" do
-      visit_page(LoginPage).login_with('foo', 'bar')
-      #DON'T ACTUALLY WANT TO LOG IN UNTIL THIS IS HANDLED SANELY
-      #@current_page.text.should include "Welcome foo"
-      @current_page.text.should include "Secure your account"
-      @current_page.text.should include "phishing"
+      on_page(LoginPage) do |page|
+        page.login_with('foo', 'bar')
+        #page.text.should include "Welcome foo"
+        page.text.should include "Secure your account"
+        page.text.should include "phishing"
+      end
     end
   end
 
   context "unsuccessful login" do
     it "should display an error messge" do
-      visit_page(LoginPage).login_with('foo', 'badpass')
-      @current_page.text.should include "Login error"
-      @current_page.text.should include "Secure your account"
+      on_page(LoginPage) do |page|
+        page.login_with('foo', 'badpass')
+        page.text.should include "Login error"
+        page.text.should include "Secure your account"
+      end
     end
   end
   
   context "check login page links" do
     it "should demonstrate all the defined links exist" do 
-      visit_page(LoginPage)
-      @current_page.phishing_element.should be_true
-      visit_page(LoginPage)
-      @current_page.password_strength_element.should be_true
+      on_page(LoginPage) do |page|
+        page.phishing_element.should be_true
+        page.password_strength_element.should be_true
+      end
     end
   end
   
   context "visit login page links" do
     it "should follow all the defined links" do 
-      visit_page(LoginPage)
-      @current_page.phishing.should be_true
-      @current_page.text.should include "Not to be confused with"
-      visit_page(LoginPage)
-      @current_page.password_strength.should be_true
-      @current_page.text.should include "measure of the effectiveness"
+      on_page(LoginPage) do |page|
+        page.phishing.should be_true
+        page.text.should include "Not to be confused with"
+      end
+      visit_page(LoginPage) do |page|
+        page.password_strength.should be_true
+        page.text.should include "measure of the effectiveness"
+      end
     end
   end
   
